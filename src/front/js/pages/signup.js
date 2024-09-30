86ea7b7cad3b85297739cc07249be6a651344b47
JiHee Ahn <jiheeahn1222@gmail.com>
2024-09-30 17:05:08 +0000

    signup page actions are done

--- src/front/js/pages/signup.js
+++ src/front/js/pages/signup.js
@@ -1,26 +1,48 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
-import { Link, useParams } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { Context } from "../store/appContext";
 
-export const Signup = props => {
-	const { store, actions } = useContext(Context);
-	const params = useParams();
+export const Signup = () => {
+	const { actions } = useContext(Context);
+	const navigate = useNavigate()
+
+	const handleSubmit = async (event) => {
+		event.preventDefault();
+
+		let email = event.target.emailInput.value;
+		let password = event.target.passwordInput.value;
+		let confirmPassword = event.target.confirmPasswordInput.value;
+
+		if (password !== confirmPassword) {
+			alert('Password does not match');
+			return;
+		}
+
+		const response = await actions.signup(email, password);
+		if (response) {
+			alert('Account has been created');
+			navigate('/login')
+		} else {
+			alert("We weren't able to sign you up at this time. Please try again later.");
+		}
+	}
+
 
 	return (
 		<div className="container mt-5">
-			<form className="row g-3">
+			<form onSubmit={handleSubmit} className="row g-3">
 				<div className="col-md-6">
 					<label for="inputEmail4" className="form-label">Email</label>
-					<input type="email" className="form-control" id="inputEmail4"/>
+					<input name='emailInput' type="email" className="form-control" id="inputEmail4" required/>
 				</div>
 				<div className="col-md-6">
 					<label for="inputPassword4" className="form-label">Password</label>
-					<input type="password" className="form-control" id="inputPassword4"/>
+					<input type="password" name='passwordInput' className="form-control" id="inputPassword4" required />
 				</div>
 				<div className="col-md-6">
 					<label for="inputConfirmPassword4" className="form-label">Confirm Password</label>
-					<input type="password" className="form-control" id="confirmPassword4"/>
+					<input type="password" name='confirmPasswordInput' className="form-control" id="confirmPassword4" required/>
 				</div>
 				<div className="col-12">
 					<div className="form-check">
